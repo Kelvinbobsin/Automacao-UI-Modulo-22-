7d798e79ea41f7f7be1cf9d86d00d6396a60c9fa
Kelvin Bobsin <kelvin.bobsin.1999@gmail.com>
2024-03-29 09:26:10 -0300

    pageobjects

--- cypress/e2e/pageObjectsTest.cy.js
+++ cypress/e2e/pageObjectsTest.cy.js
@@ -1,16 +1,21 @@
 /// <reference types="cypress" />
 
-const loginPage = require("../support/pages/login.page");
-const data = require("../fixtures/data.json")
+const { homePage } = require("../support/pages/home.page")
+const loginPage = require("../support/pages/login.page")
+const { email, senha } = require('../fixtures/data.json')
+const { profilePage } = require("../support/pages/profile.page")
 
+describe('Teste de Autenticação', () => {
 
-describe('Acesse o painel de administração', () => {
-    beforeEach(() => {
-        cy.visit('/wp-admin')
-    })
+  beforeEach(() => {
+    cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' })
+    cy.visit('/')
+  })
 
-    it('Deve fazer login com credenciais válidas', () => {
-        loginPage.login(data.usuario, data.senha)
-        
-    });
-});
+  it('deve fazer o login com sucesso', () => {
+    homePage.openMenu('Account')
+    loginPage.login(email, senha)
+    homePage.openMenu('Account')
+    profilePage.customerName().should('have.text', 'EBAC Cliente')
+  })
+})
