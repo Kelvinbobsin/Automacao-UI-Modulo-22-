35ce8963a8652fa18b4facb7989a25e8fd063c09
Kelvin Bobsin <kelvin.bobsin.1999@gmail.com>
2024-03-29 09:50:59 -0300

    BDD

--- cypress/e2e/BDD.cy.js
+++ cypress/e2e/BDD.cy.js
@@ -1,21 +1,18 @@
 /// <reference types="cypress" />
 
-const { homePage } = require("../support/pages/home.page")
-const loginPage = require("../support/pages/login.page")
 const { email, senha } = require('../fixtures/data.json')
 const { profilePage } = require("../support/pages/profile.page")
 
-describe('Teste de Autenticação', () => {
+context('Dado que estou na pagina inicial', ()=>{
+  context('Quando eu faço login', ()=>{
+    beforeEach(()=>{
+      cy.login(email, senha)
+    })
 
-  beforeEach(() => {
-    cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' })
-    cy.visit('/')
+    it('Então o nome do usuário deve aparecer na página de perfil', () => {
+      profilePage.customerName().should('have.text', 'EBAC Cliente')
+    })
+  
   })
 
-  it('deve fazer o login com sucesso', () => {
-    homePage.openMenu('Account')
-    loginPage.login(email, senha)
-    homePage.openMenu('Account')
-    profilePage.customerName().should('have.text', 'EBAC Cliente')
-  })
 })
